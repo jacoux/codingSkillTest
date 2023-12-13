19a3a65f2247cccae5c56a2facb4216b37a3df82
nielsjacobs <niels@zimmo.be>
2023-12-13 14:34:53 +0100

    basic test

--- src/app/contact/components/contact-form/contact-form.component.spec.ts
+++ src/app/contact/components/contact-form/contact-form.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { ContactFormComponent } from './contact-form.component';
 
 describe('ContactFormComponent', () => {
@@ -8,9 +8,9 @@ describe('ContactFormComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ContactFormComponent ]
-    })
-    .compileComponents();
+      declarations: [ContactFormComponent],
+      imports: [ReactiveFormsModule],
+    }).compileComponents();
   });
 
   beforeEach(() => {
@@ -21,5 +21,23 @@ describe('ContactFormComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should disable submit button when the form is invalid', () => {
+    component.contactForm.setValue({
+      firstName: '',
+      email: '',
+    });
+    expect(component.contactForm.valid).toBeFalsy();
+    const submitButton = fixture.nativeElement.querySelector('button');
+    expect(submitButton.disabled).toBeTruthy();
+  });
+
+  it('should call onSubmit method on form submission', () => {
+    spyOn(component, 'onSubmit');
+    const form = fixture.nativeElement.querySelector('form');
+    form.dispatchEvent(new Event('submit'));
+    fixture.detectChanges();
+    expect(component.onSubmit).toHaveBeenCalled();
   });
 });
